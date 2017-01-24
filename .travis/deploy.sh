b305705b9b2e43601c64820cf5e5872100f448f8
Kuldeep Ghogre <kuldeep.ghogre_external@immobilienscout24.de>
2017-01-24 15:40:16 +0100

    adding travis configuration for older version

---
+++ .travis/deploy.sh
@@ -0,0 +1,6 @@
+#!/bin/bash
+set -ev
+if [ -n "${TRAVIS_TAG}" ]; then
+  git reset --hard HEAD
+  ./gradlew publishPlugins -Pversion="${TRAVIS_TAG}" -Dgradle.publish.key=$GRADLE_KEY -Dgradle.publish.secret=$GRADLE_SECRET
+fi
