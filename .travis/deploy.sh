6525fc27fc9c86bb5366af7aada4b2c03b360fc9
Kuldeep Ghogre <kuldeep.ghogre@gmail.com>
2017-01-24 08:03:55 +0100

    modifying build configuration

--- .travis/deploy.sh
+++ .travis/deploy.sh
@@ -2,5 +2,5 @@
 set -ev
 if [ -n "${TRAVIS_TAG}" ]; then
   ./gradlew release -Prelease.useAutomaticVersion=true -Prelease.releaseVersion="${TRAVIS_TAG}" -Prelease.newVersion="${TRAVIS_TAG}"-SNAPSHOT
-  ./gradlew publishPlugins -Pversion="${TRAVIS_TAG}"
+  ./gradlew publishPlugins -Pversion="${TRAVIS_TAG}" -Pgradle.publish.key="${GRADLE_KEY}" -Pgradle.publish.secret="GRADLE_SECRET"
 fi
