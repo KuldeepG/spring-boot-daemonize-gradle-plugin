1e1d39211cda7a562ccf400cec0ca42e2e87d357
Kuldeep Ghogre <kuldeep.ghogre_external@immobilienscout24.de>
2017-01-24 13:02:52 +0100

    Temporarily removing the release step

--- .travis/deploy.sh
+++ .travis/deploy.sh
@@ -2,6 +2,5 @@
 set -ev
 if [ -n "${TRAVIS_TAG}" ]; then
   git reset --hard HEAD
-  ./gradlew release -Prelease.useAutomaticVersion=true -Prelease.releaseVersion="${TRAVIS_TAG}" -Prelease.newVersion="${TRAVIS_TAG}"-SNAPSHOT
   ./gradlew publishPlugins -Pversion="${TRAVIS_TAG}" -Pgradle.publish.key="${GRADLE_KEY}" -Pgradle.publish.secret="${GRADLE_SECRET}"
 fi
