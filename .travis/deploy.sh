37a65064a3ba5b3b40a601242d37608644b97eea
Kuldeep Ghogre <kuldeep.ghogre_external@immobilienscout24.de>
2017-01-24 11:43:28 +0100

    Fixing deploy script

--- .travis/deploy.sh
+++ .travis/deploy.sh
@@ -1,7 +1,6 @@
 #!/bin/bash
 set -ev
 if [ -n "${TRAVIS_TAG}" ]; then
-  cd $HOME 
-  ./gradlew release -Prelease.useAutomaticVersion=true -Prelease.releaseVersion="${TRAVIS_TAG}" -Prelease.newVersion="${TRAVIS_TAG}"-SNAPSHOT
-  ./gradlew publishPlugins -Pversion="${TRAVIS_TAG}" -Pgradle.publish.key="${GRADLE_KEY}" -Pgradle.publish.secret="GRADLE_SECRET"
+  $HOME/gradlew -b $HOME/build.gradle release -Prelease.useAutomaticVersion=true -Prelease.releaseVersion="${TRAVIS_TAG}" -Prelease.newVersion="${TRAVIS_TAG}"-SNAPSHOT
+  $HOME/gradlew -b $HOME/build.gradle publishPlugins -Pversion="${TRAVIS_TAG}" -Pgradle.publish.key="${GRADLE_KEY}" -Pgradle.publish.secret="${GRADLE_SECRET}"
 fi
