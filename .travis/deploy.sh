9ba0268489170a800959d3b3cbe685c148ec0a12
Kuldeep Ghogre <kuldeep.ghogre_external@immobilienscout24.de>
2017-01-24 13:16:18 +0100

    Fixing version number

--- .travis/deploy.sh
+++ .travis/deploy.sh
@@ -2,5 +2,5 @@
 set -ev
 if [ -n "${TRAVIS_TAG}" ]; then
   git reset --hard HEAD
-  ./gradlew publishPlugins -Pversion='${TRAVIS_TAG}' -Dgradle.publish.key='${GRADLE_KEY}' -Dgradle.publish.secret='${GRADLE_SECRET}'
+  ./gradlew publishPlugins -Pversion="${TRAVIS_TAG}" -Dgradle.publish.key='${GRADLE_KEY}' -Dgradle.publish.secret='${GRADLE_SECRET}'
 fi
