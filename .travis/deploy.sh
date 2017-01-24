375c924bf1856264b9f4f2556b1578c9deb3facb
Kuldeep Ghogre <kuldeep.ghogre_external@immobilienscout24.de>
2017-01-24 10:01:26 +0100

    Removing logging information and updating deploy script

--- .travis/deploy.sh
+++ .travis/deploy.sh
@@ -1,6 +1,7 @@
 #!/bin/bash
 set -ev
 if [ -n "${TRAVIS_TAG}" ]; then
-  ./gradlew release -Prelease.useAutomaticVersion=true -Prelease.releaseVersion="${TRAVIS_TAG}" -Prelease.newVersion="${TRAVIS_TAG}"-SNAPSHOT --full-stacktrace
-  ./gradlew publishPlugins -Pversion="${TRAVIS_TAG}" -Pgradle.publish.key="${GRADLE_KEY}" -Pgradle.publish.secret="GRADLE_SECRET" --full-stacktrace
+  cd $HOME 
+  ./gradlew release -Prelease.useAutomaticVersion=true -Prelease.releaseVersion="${TRAVIS_TAG}" -Prelease.newVersion="${TRAVIS_TAG}"-SNAPSHOT
+  ./gradlew publishPlugins -Pversion="${TRAVIS_TAG}" -Pgradle.publish.key="${GRADLE_KEY}" -Pgradle.publish.secret="GRADLE_SECRET"
 fi
